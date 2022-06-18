f259cd5d739990d0c130573439f62acd8694f8ef
Faruk Cesur <faruk-cesur@hotmail.com>
2022-06-18 15:49:09 +0300

    Added Winter Zone Mini Package & Updated FX

---
+++ Assets/Winter Zone Mini 1.0/Scripts/FXDemoController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FXDemoController : MonoBehaviour
+{
+    public GameObject StormFX;
+    public GameObject BlizzardFX;
+    public GameObject SnowFX;
+    public GameObject AudioFX;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+            Storm();
+        if (Input.GetKeyUp(KeyCode.T))
+            Blizzard();
+        if (Input.GetKeyUp(KeyCode.Y))
+            Snow();
+        if (Input.GetKeyUp(KeyCode.U))
+            TurnOffFX();
+    }
+
+    public void Storm()
+    {
+        TurnOffFX();
+        StormFX.SetActive(true);
+        AudioFX.SetActive(true);
+    }
+
+    public void Blizzard()
+    {
+        TurnOffFX();
+        BlizzardFX.SetActive(true);
+        AudioFX.SetActive(true);
+    }
+
+    public void Snow()
+    {
+        TurnOffFX();
+        SnowFX.SetActive(true);
+        AudioFX.SetActive(true);
+    }
+
+    public void TurnOffFX()
+    {
+        StormFX.SetActive(false);
+        BlizzardFX.SetActive(false);
+        SnowFX.SetActive(false);
+        AudioFX.SetActive(false);
+    }
+}
